a865452cbfa1a2079ba9062c2e6b37d120ef8c26
vinitghaturle <vinitghaturle656@gmail.com>
2025-08-22 03:15:09 +0530

    update 7

--- src/UberSplineComponent.jsx
+++ src/UberSplineComponent.jsx
@@ -3,17 +3,30 @@ import React, { useRef } from 'react';
 const UberSplineComponent = ({ onSplineLoaded }) => {
   const iframeRef = useRef(null);
 
-  // Call onSplineLoaded when iframe loads
-  const handleLoad = () => {
-    if (onSplineLoaded) onSplineLoaded();
+  // Forward wheel events to parent
+  const handleWheel = (e) => {
+    window.scrollBy({ top: e.deltaY, behavior: 'smooth' });
   };
 
   return (
     <div className="min-h-screen text-white bg-[#02001b]">
       <div className="max-w-100vw max-h-100vh ">
         <main className="bg-[#02001b] min-h-100vh rounded-xl overflow-hidden shadow-2xl">
-          {/* Spline Iframe */}
-          <div className="relative block">
+          <div className="relative block" style={{ position: "relative" }}>
+            {/* Overlay div to capture scroll */}
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                zIndex: 2,
+                cursor: "default",
+                background: "transparent",
+              }}
+              onWheel={handleWheel}
+            />
             <iframe 
               ref={iframeRef}
               src='https://my.spline.design/claritystream-1HFcWDI96cHXt90ipWYvYZiz/'
@@ -24,7 +37,8 @@ const UberSplineComponent = ({ onSplineLoaded }) => {
               className="w-full h-96 md:h-[500px] lg:h-[600px]"
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
-              onLoad={handleLoad}
+              style={{ position: "relative", zIndex: 1 }}
+              onLoad={onSplineLoaded}
             />
           </div>
         </main>
